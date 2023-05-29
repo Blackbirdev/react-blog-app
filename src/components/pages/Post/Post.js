5c26590333f44ae26e167503e4f0a0d490ab4ea9
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-05-29 17:55:33 +0200

    Add pages with routes

---
+++ src/components/pages/Post/Post.js
@@ -0,0 +1,9 @@
+const Post = () => {
+    return (
+        <>
+            <h1>SinglePost</h1>
+        </>
+    )
+}
+
+export default Post;
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,3 +1,8 @@
-const initialState = {}
+const initialState = {
+
+    posts: [
+        {},
+    ]
+}
 
 export default initialState;
