330b4c70887f2c247a73e7eed3b3444d4828a12a
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-07-01 23:29:25 +0200

    Fix bug with title and author placeholder

--- src/components/common/features/PostForm/PostForm.js
+++ src/components/common/features/PostForm/PostForm.js
@@ -32,21 +32,28 @@ const PostForm = ({ action, actionText, ...props }) => {
                     <h2 className='py-3'>Add Post</h2>
                     <Form.Group className="mb-4" controlId="formBasicEmail">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control {...register("title", { required: true, minLength: 3 })} value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Enter title" />
+                        <Form.Control
+                            {...register("title", { required: true, minLength: 3 })}
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                            type="text" placeholder="Enter title" />
                         {errors.title && <small className="d-block form-text text-danger mt-2">Title is too short (required min. 3)</small>}
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>Author</Form.Label>
                         <Form.Control
-                            {...register("title", { required: true, minLength: 3 })}
-                            value={title}
+                            {...register("author", { required: true, minLength: 3 })}
+                            value={author}
                             onChange={e => setAuthor(e.target.value)}
                             type="text" placeholder="Enter author" />
                         {errors.title && <small className="d-block form-text text-danger mt-2">Oh no! Author's name is too short (required min. 3)</small>}
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label style={{ width: '80%', border: '' }}>Published date</Form.Label>
-                        <ReactDatePicker dateFormat="dd-MM-yyyy" selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
+                        <ReactDatePicker
+                            dateFormat="dd-MM-yyyy"
+                            selected={publishedDate}
+                            onChange={(date) => setPublishedDate(date)} />
                         {dateError && <small className="d-block form-text text-danger mt-2">Published date is required</small>}
                     </Form.Group>
                     <Form.Group className="mb-4">
@@ -60,7 +67,10 @@ const PostForm = ({ action, actionText, ...props }) => {
                     </Form.Group>
                     <Form.Group className="mb-4">
                         <Form.Label>Content of the post</Form.Label>
-                        <ReactQuill placeholder="Leave a comment here" value={content} onChange={setContent} />
+                        <ReactQuill
+                            placeholder="Leave a comment here"
+                            value={content}
+                            onChange={setContent} />
                         {contentError && <small className="d-block form-text text-danger mt-2">Content of the post can't be empty</small>}
                     </Form.Group>
                     <Button style={{ width: '20%' }} variant='primary' type='submit'>{actionText}</Button>
